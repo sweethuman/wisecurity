d55b41f457406354e2c2eacad2a2d8bf2881a5ca
Arsuh <arsene.vlad@gmail.com>
2019-08-05 12:48:34 +0300

    Add Gradients

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-app-bar app class="curiosity-blue" dark>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Wi</span>
+        <span class="font-weight-light">SECURITY</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
