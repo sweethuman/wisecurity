084051debd5580bb2c0e3abefcb21323a5e53182
Vlad <vlad.schiller123@gmail.com>
2019-08-05 13:14:31 +0300

    Add Icons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <v-app>
     <v-app-bar app class="curiosity-blue" dark>
-      <v-toolbar-title class="headline text-uppercase">
+      <v-toolbar-title class="headline text-uppercase mr-5">
         <span>Wi</span>
         <span class="font-weight-light">SECURITY</span>
       </v-toolbar-title>
       <v-toolbar-items>
-        <v-btn text to="/">HOME</v-btn>
-        <v-btn text to="/info">INFO</v-btn>
-        <v-btn text to="/aplic">APLIC</v-btn>
-        <v-btn text to="/about">ABOUT</v-btn>
+        <v-btn text to="/">
+          <v-icon left>mdi-home</v-icon>HOME
+        </v-btn>
+        <v-btn text to="/info">
+          <v-icon left>mdi-laptop</v-icon>INFO
+        </v-btn>
+        <v-btn text to="/aplic">
+          <v-icon left>mdi-fuse</v-icon>APLIC
+        </v-btn>
+        <v-btn text to="/about">
+          <v-icon left>mdi-information</v-icon>ABOUT
+        </v-btn>
       </v-toolbar-items>
 
       <v-spacer></v-spacer>
