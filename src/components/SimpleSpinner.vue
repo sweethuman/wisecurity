0b5d5c6deca04c2661f1bbfaafa08f9483a93296
sweethuman <hackscomicon@yahoo.com>
2019-08-05 21:56:35 +0300

    Add Simple Spinner

---
+++ src/components/SimpleSpinner.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-progress-circular indeterminate size="50" color="primary" class="py-12"></v-progress-circular>
+</template>
+
+<script>
+export default {
+  name: "SimpleSpinner",
+};
+</script>
+
+<style scoped></style>
