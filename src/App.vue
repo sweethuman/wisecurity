f811d9a03a97f02745b173eb86cb0c5ec475eb52
Vlad <vlad.schiller123@gmail.com>
2019-08-05 12:59:55 +0300

    Add App Bar Buttons and Router View

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
         <span>Wi</span>
         <span class="font-weight-light">SECURITY</span>
       </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn text to="/">HOME</v-btn>
+        <v-btn text to="/info">INFO</v-btn>
+        <v-btn text to="/aplic">APLIC</v-btn>
+        <v-btn text to="/about">ABOUT</v-btn>
+      </v-toolbar-items>
+
       <v-spacer></v-spacer>
       <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
@@ -12,7 +19,7 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
