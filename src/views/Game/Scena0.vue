05edd785bd427d0bbbb9ae1e4afc717b0d6c9b38
sweethuman <hackscomicon@yahoo.com>
2019-08-06 10:29:41 +0300

    Update Browser Game

--- src/views/Game/Scena0.vue
+++ src/views/Game/Scena0.vue
@@ -3,7 +3,10 @@
     <v-layout fill-height align-center justify-center>
       <v-flex lg8 md10 xs12>
         <v-card>
-          <v-card-title>Start</v-card-title>
+          <v-img
+            src="https://images.unsplash.com/photo-1541535881962-3bb380b08458?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2116&q=80"
+            aspect-ratio="2"
+          ></v-img>
           <v-card-text class="display-2">
             Prietenul tău Simon are nevoie de ajutor cu contul lui de E-mail. Lui i se tot trimit mail-uri fără ca el să
             le fi scris. Intri la el acasă și îi strângi mâna.Ce faci mai departe?
